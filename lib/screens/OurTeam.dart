6d5fce6009d78f5533e16fbe79acf53fe9bcaaee
Adnanali777 <transcend777ice@gmail.com>
2020-10-06 23:18:43 +0530

    added profiles to team screen

--- lib/screens/OurTeam.dart
+++ lib/screens/OurTeam.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class dscTeam extends StatefulWidget {
   @override
@@ -9,8 +10,69 @@ class _dscTeamState extends State<dscTeam> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Our Team'),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 20,horizontal: 10),
+                  margin: EdgeInsets.fromLTRB(17, 30, 17, 10),
+                  height: MediaQuery.of(context).size.height*0.47,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 7.0,
+                      ),
+                    ],
+                  ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(radius: 50,backgroundImage: AssetImage('assets/mansi_goel.jpg'),),
+                          SizedBox(height: 10,),
+                          Text('Mansi Goel',style: TextStyle(fontSize: 20,letterSpacing: 0.9,color: Colors.black),),
+                          SizedBox(height: 12,),
+                          Text('DSC LEAD',style: TextStyle(fontSize: 15,letterSpacing: 0.3,color: Colors.grey[600]),),
+                          SizedBox(height: 17,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Material(
+                                child: InkWell(
+                                  onTap: (){},
+                                  child: Icon(AntDesign.linkedin_square,size: 20,),
+                                ),
+                              ),
+                              SizedBox(width: 28,),
+                              GestureDetector(
+                                onTap: (){},
+                                child: Icon(AntDesign.github,size: 20,),
+                              ),
+                              SizedBox(width: 28,),
+                              GestureDetector(
+                                onTap: (){},
+                                child: Icon(AntDesign.twitter,size: 20,),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 30,),
+                          Expanded(
+                            child: Container(
+                              child: Text('Trainee at bosch rexroth. LEAD at DSC by Google developers.Core team member at (techno-managerial society) horizon, core team member at DSC-AKGEC. Done internship of web development. Knowledge about pneumatics, hydraulics, plc.',style: TextStyle(fontSize: 13,color: Colors.grey[600],wordSpacing: 2.3),textAlign: TextAlign.center,softWrap: true,),
+                            ),
+                          ),
+                        ],
+                    ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
