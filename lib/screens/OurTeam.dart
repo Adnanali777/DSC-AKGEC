eb000e1cbf2e6a8cbaa632e616739581ef153e9f
Adnanali777 <transcend777ice@gmail.com>
2020-10-14 00:10:26 +0530

    added mixin class to keep the grids alive so as they not animate as we scroll back in the grid

--- lib/screens/OurTeam.dart
+++ lib/screens/OurTeam.dart
@@ -12,50 +12,10 @@ class dscTeam extends StatefulWidget {
   _dscTeamState createState() => _dscTeamState();
 }
 
-class _dscTeamState extends State<dscTeam> with SingleTickerProviderStateMixin{
-  Animation animationlead;
-  Animation animationteam;
-  AnimationController animationControllerlead;
+class _dscTeamState extends State<dscTeam> with AutomaticKeepAliveClientMixin {
   @override
-  void initState() {
-    super.initState();
-    animationControllerlead = AnimationController(
-      duration: Duration(milliseconds: 600),
-      vsync: this,
-    );
-    animationlead = Tween(begin: -1.0 , end: 0).animate(CurvedAnimation(
-      parent: animationControllerlead,
-      curve: Interval(0.5,1.0,curve:Curves.fastOutSlowIn),
-    ));
-    animationteam = Tween<double>(begin: 1,end: 0).animate(CurvedAnimation(
-      parent: animationControllerlead,
-      curve: Interval(0.7,1.0,curve: Curves.fastOutSlowIn),
-    ));
-    animationControllerlead.forward();
-  }
-  List<MemberLayout> members = [
-    MemberLayout(img: 'assets/user.png',name: 'Apoorv Jain',memberType: 'Core Member',domain: 'Machine Learning'),
-    MemberLayout(img: 'assets/user.png',name: 'Tarun Raghav',memberType: 'Core Member',domain: 'Machine Learning'),
-    MemberLayout(img: 'assets/user.png',name: 'Pranjul Itondia',memberType: 'Core Member',domain: 'Machine Learning'),
-    MemberLayout(img: 'assets/user.png',name: 'Nihal Choudhary',memberType: 'Core Member',domain: 'Machine Learning'),
-    MemberLayout(img: 'assets/user.png',name: 'Vishesh Kushwaha',memberType: 'Core Member',domain: 'Machine Learning'),
-    MemberLayout(img: 'assets/user.png',name: 'Krishna Agrawal',memberType: 'Core Member',domain: 'Web Development'),
-    MemberLayout(img: 'assets/user.png',name: 'Hitesh Tripathi',memberType: 'Core Member',domain: 'Web Development'),
-    MemberLayout(img: 'assets/user.png',name: 'Shivam Bisht',memberType: 'Core Member',domain: 'Web Development'),
-    MemberLayout(img: 'assets/user.png',name: 'Himani Chauhan',memberType: 'Core Member',domain: 'Web Development'),
-    MemberLayout(img: 'assets/user.png',name: 'Vishesh Dhawan',memberType: 'Core Member',domain: 'Web Development'),
-    MemberLayout(img: 'assets/user.png',name: 'Anshika Bajpai',memberType: 'Core Member',domain: 'App Development'),
-    MemberLayout(img: 'assets/user.png',name: 'Nirbhay Shukla',memberType: 'Core Member',domain: 'App Development'),
-    MemberLayout(img: 'assets/user.png',name: 'Rishabh Singh',memberType: 'Core Member',domain: 'App Development'),
-    MemberLayout(img: 'assets/user.png',name:'Shatakshi Upadhyay' ,memberType: 'Core Member',domain: 'Managerial',),
-    MemberLayout(img: 'assets/user.png',name:'Ayushi Bansal' ,memberType: 'Core Member',domain: 'Managerial',),
-    MemberLayout(img: 'assets/user.png',name:'Pranjal Maurya' ,memberType: 'Core Member',domain: 'Managerial',),
-  ];
-  @override
-  void dispose() {
-    super.dispose();
-    animationControllerlead.dispose();
-  }
+  bool get wantKeepAlive => true;
+
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
@@ -76,121 +36,156 @@ class _dscTeamState extends State<dscTeam> with SingleTickerProviderStateMixin{
                   itemExtent: 400,
                   delegate: SliverChildListDelegate(
                     [
-                      AnimatedBuilder(
-                        animation: animationControllerlead,
-                        builder: (BuildContext context,Widget child){
-                          return Transform(
-                            transform: Matrix4.translationValues(0, animationlead.value*width, 0),
-                            child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                              margin: EdgeInsets.fromLTRB(17, 30, 17, 15),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(15),
-                                border: Border.all(width: 1.0,color: Colors.grey[400],style: BorderStyle.solid),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey[500],
-                                    blurRadius: 5.0,
-                                    offset: Offset(5,7),
-                                  ),
-                                ],
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  CircleAvatar(
-                                    radius: 50, backgroundImage: AssetImage(
-                                      'assets/mansi_goel.jpg'),),
-                                  SizedBox(height: 10,),
-                                  Text('Mansi Goel', style: TextStyle(fontSize: 20,
-                                      letterSpacing: 0.9,
-                                      color: Colors.black),),
-                                  SizedBox(height: 12,),
-                                  Text('DSC LEAD', style: TextStyle(fontSize: 15,
-                                      letterSpacing: 0.3,
-                                      color: Colors.grey[600]),),
-                                  SizedBox(height: 17,),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      GestureDetector(
-                                          onTap: ()async{
-                                            const url = 'https://www.linkedin.com/feed/';
-                                            if (await canLaunch(url)) {
-                                              await launch(url);
-                                            } else {
-                                              throw 'Could not launch $url';
-                                            }
-                                          },
-                                          child: linkedIn),
-                                      SizedBox(width: 39,),
-                                      GestureDetector(
-                                          onTap: ()async{
-                                            const url = 'https://github.com/';
-                                            if (await canLaunch(url)) {
-                                              await launch(url);
-                                            } else {
-                                              throw 'Could not launch $url';
-                                            }
-                                          },
-                                          child: github),
-                                      SizedBox(width: 39,),
-                                      GestureDetector(
-                                          onTap: ()async{
-                                            const url = 'https://twitter.com/home?lang=en';
-                                            if (await canLaunch(url)) {
-                                              await launch(url);
-                                            } else {
-                                              throw 'Could not launch $url';
-                                            }
-                                          },
-                                          child: twitter),
-                                    ],
-                                  ),
-                                  SizedBox(height: 30,),
-                                  Expanded(
-                                    child: Container(
-                                      child: Text(
-                                        'Trainee at bosch rexroth. LEAD at DSC by Google developers.Core team member at (techno-managerial society) horizon, core team member at DSC-AKGEC. Done internship of web development. Knowledge about pneumatics, hydraulics, plc.',
-                                        style: TextStyle(fontSize: 13,
-                                            color: Colors.grey[600],
-                                            wordSpacing: 2.3),
-                                        textAlign: TextAlign.center,
-                                        softWrap: true,),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-
+                  TweenAnimationBuilder(
+                    child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    margin: EdgeInsets.fromLTRB(17, 30, 17, 15),
+                    decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(width: 1.0,color: Colors.grey[400],style: BorderStyle.solid),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[500],
+                        blurRadius: 5.0,
+                        offset: Offset(5,7),
                       ),
+                    ],
+                    ),
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 50, backgroundImage: AssetImage(
+                          'assets/mansi_goel.jpg'),),
+                      SizedBox(height: 10,),
+                      Text('Mansi Goel', style: TextStyle(fontSize: 20,
+                          letterSpacing: 0.9,
+                          color: Colors.black),),
+                      SizedBox(height: 12,),
+                      Text('DSC LEAD', style: TextStyle(fontSize: 15,
+                          letterSpacing: 0.3,
+                          color: Colors.grey[600]),),
+                      SizedBox(height: 17,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                              onTap: ()async{
+                                const url = 'https://www.linkedin.com/feed/';
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              },
+                              child: linkedIn),
+                          SizedBox(width: 39,),
+                          GestureDetector(
+                              onTap: ()async{
+                                const url = 'https://github.com/';
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              },
+                              child: github),
+                          SizedBox(width: 39,),
+                          GestureDetector(
+                              onTap: ()async{
+                                const url = 'https://twitter.com/home?lang=en';
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              },
+                              child: twitter),
+                        ],
+                      ),
+                      SizedBox(height: 30,),
+                      Expanded(
+                        child: Container(
+                          child: Text(
+                            'Trainee at bosch rexroth. LEAD at DSC by Google developers.Core team member at (techno-managerial society) horizon, core team member at DSC-AKGEC. Done internship of web development. Knowledge about pneumatics, hydraulics, plc.',
+                            style: TextStyle(fontSize: 13,
+                                color: Colors.grey[600],
+                                wordSpacing: 2.3),
+                            textAlign: TextAlign.center,
+                            softWrap: true,),
+                        ),
+                      ),
+                    ],
+                    ),
+                ),
+                    duration: Duration(milliseconds: 600),
+                    tween: Tween<double>(begin: -1.0 , end: 0),
+                    curve: Interval(0.5,1,curve:Curves.fastOutSlowIn),
+                    builder: (BuildContext context , double _val , Widget child) {
+                      return Transform(
+                        transform: Matrix4.translationValues(0, _val * width,
+                            0.0),
+                        child: child,
+                      );
+                    }
+                  ),
 
                     ],)
               ),
-              SliverPadding(
-                padding: EdgeInsets.symmetric(horizontal: 17,vertical: 5),
-                sliver: SliverGrid(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 3 / 4,
-                    mainAxisSpacing: 20,
-                    crossAxisSpacing: 20,
-                  ),
-                  delegate: SliverChildBuilderDelegate((BuildContext context,
-                      int index) {
-                    return members[index];
-                  },
-                    childCount: members.length,
-                  ),
-                ),
-              ),
+              membergrid(),
             ],
           ),
         ),
       ),
     );
   }
+}
+class membergrid extends StatefulWidget {
+  @override
+  _membergridState createState() => _membergridState();
+}
+
+class _membergridState extends State<membergrid> with AutomaticKeepAliveClientMixin{
+  List<MemberLayout> members = [
+    MemberLayout(img: 'assets/user.png',name: 'Apoorv Jain',memberType: 'Core Member',domain: 'Machine Learning'),
+    MemberLayout(img: 'assets/user.png',name: 'Tarun Raghav',memberType: 'Core Member',domain: 'Machine Learning'),
+    MemberLayout(img: 'assets/user.png',name: 'Pranjul Itondia',memberType: 'Core Member',domain: 'Machine Learning'),
+    MemberLayout(img: 'assets/user.png',name: 'Nihal Choudhary',memberType: 'Core Member',domain: 'Machine Learning'),
+    MemberLayout(img: 'assets/user.png',name: 'Vishesh Kushwaha',memberType: 'Core Member',domain: 'Machine Learning'),
+    MemberLayout(img: 'assets/user.png',name: 'Krishna Agrawal',memberType: 'Core Member',domain: 'Web Development'),
+    MemberLayout(img: 'assets/user.png',name: 'Hitesh Tripathi',memberType: 'Core Member',domain: 'Web Development'),
+    MemberLayout(img: 'assets/user.png',name: 'Shivam Bisht',memberType: 'Core Member',domain: 'Web Development'),
+    MemberLayout(img: 'assets/user.png',name: 'Himani Chauhan',memberType: 'Core Member',domain: 'Web Development'),
+    MemberLayout(img: 'assets/user.png',name: 'Vishesh Dhawan',memberType: 'Core Member',domain: 'Web Development'),
+    MemberLayout(img: 'assets/user.png',name: 'Anshika Bajpai',memberType: 'Core Member',domain: 'App Development'),
+    MemberLayout(img: 'assets/user.png',name: 'Nirbhay Shukla',memberType: 'Core Member',domain: 'App Development'),
+    MemberLayout(img: 'assets/user.png',name: 'Rishabh Singh',memberType: 'Core Member',domain: 'App Development'),
+    MemberLayout(img: 'assets/user.png',name:'Shatakshi Upadhyay' ,memberType: 'Core Member',domain: 'Managerial',),
+    MemberLayout(img: 'assets/user.png',name:'Ayushi Bansal' ,memberType: 'Core Member',domain: 'Managerial',),
+    MemberLayout(img: 'assets/user.png',name:'Pranjal Maurya' ,memberType: 'Core Member',domain: 'Managerial',),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 17,vertical: 5),
+      sliver: SliverGrid(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 4,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        delegate: SliverChildBuilderDelegate((BuildContext context,
+            int index) {
+          return members[index];
+        },
+          childCount: members.length,
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
--- lib/shared/team-details.dart
+++ lib/shared/team-details.dart
@@ -15,103 +15,89 @@ class MemberLayout extends StatefulWidget {
   _MemberLayoutState createState() => _MemberLayoutState();
 }
 
-class _MemberLayoutState extends State<MemberLayout> with SingleTickerProviderStateMixin{
-  Animation animationteam;
-  AnimationController animationControllerteam;
+class _MemberLayoutState extends State<MemberLayout> with AutomaticKeepAliveClientMixin{
   @override
-  void initState() {
-    super.initState();
-    animationControllerteam = AnimationController(
-      duration: Duration(milliseconds: 800),
-      vsync: this,
-    );
-    animationteam = Tween<double>(begin: 1,end: 0).animate(CurvedAnimation(
-      parent: animationControllerteam,
-      curve: Interval(0.7,1.0,curve: Curves.fastOutSlowIn),
-    ));
-    animationControllerteam.forward();
-    animationControllerteam.addStatusListener((status) {
-      if (status == AnimationStatus.completed){
-        animationControllerteam.stop(canceled: true);
-      }
-    });
-  }
+  bool get wantKeepAlive => true;
 
-  @override
+
 
 
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
-    return AnimatedBuilder(
-      animation: animationControllerteam,
-      builder: (BuildContext context,_){
-        return Transform(
-          transform: Matrix4.translationValues(0, animationteam.value*width, 0),
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 15),
-            height: MediaQuery.of(context).size.height*0.3,
-            width: MediaQuery.of(context).size.width*0.43,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow:[ BoxShadow(
-                color: Colors.grey,
-                blurRadius: 3.0,
-              )],
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                CircleAvatar(radius: 35,backgroundImage: AssetImage('assets/user.png'),),
-                SizedBox(height: 10,),
-                Text(widget.name,style: TextStyle(fontSize: 16,color: Colors.black,),),
-                SizedBox(height: 8,),
-                Text(widget.memberType,style: TextStyle(color: Colors.grey[600],fontSize: 11.7),),
-                SizedBox(height: 8,),
-                Text(widget.domain,style: TextStyle(color: Colors.grey[600],fontSize: 11.7),),
-                SizedBox(height: 22,),
-                Row(mainAxisAlignment:MainAxisAlignment.center,children: [
-                  GestureDetector(
-                      onTap: ()async{
-                        const url = 'https://www.linkedin.com/feed/';
-                        if (await canLaunch(url)) {
-                        await launch(url);
-                        } else {
-                        throw 'Could not launch $url';
-                        }
-                      },
-                      child: linkedIn),
-                  SizedBox(width: 20,),
-                  GestureDetector(
-                      onTap: ()async{
-                        const url = 'https://github.com/';
-                        if (await canLaunch(url)) {
-                          await launch(url);
-                        } else {
-                          throw 'Could not launch $url';
-                        }
-                      },
-                      child: github),
-                  SizedBox(width: 20,),
-                  GestureDetector(
-                      onTap: ()async{
-                        const url = 'https://twitter.com/home?lang=en';
-                        if (await canLaunch(url)) {
-                          await launch(url);
-                        } else {
-                          throw 'Could not launch $url';
-                        }
-                      },
-                      child: twitter),
-                ],
-                ),
-              ],
-            ),
+    return TweenAnimationBuilder(
+      child: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10,vertical: 15),
+          height: MediaQuery.of(context).size.height*0.3,
+          width: MediaQuery.of(context).size.width*0.43,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow:[ BoxShadow(
+              color: Colors.grey,
+              blurRadius: 3.0,
+            )],
           ),
-        );
-      },
-
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              CircleAvatar(radius: 35,backgroundImage: AssetImage('assets/user.png'),),
+              SizedBox(height: 10,),
+              Text(widget.name,style: TextStyle(fontSize: 16,color: Colors.black,),),
+              SizedBox(height: 8,),
+              Text(widget.memberType,style: TextStyle(color: Colors.grey[600],fontSize: 11.7),),
+              SizedBox(height: 8,),
+              Text(widget.domain,style: TextStyle(color: Colors.grey[600],fontSize: 11.7),),
+              SizedBox(height: 22,),
+              Row(mainAxisAlignment:MainAxisAlignment.center,children: [
+                GestureDetector(
+                    onTap: ()async{
+                      const url = 'https://www.linkedin.com/feed/';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    child: linkedIn),
+                SizedBox(width: 20,),
+                GestureDetector(
+                    onTap: ()async{
+                      const url = 'https://github.com/';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    child: github),
+                SizedBox(width: 20,),
+                GestureDetector(
+                    onTap: ()async{
+                      const url = 'https://twitter.com/home?lang=en';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    child: twitter),
+              ],
+              ),
+            ],
+          ),
+        ),
+      ),
+        duration: Duration(milliseconds: 600),
+        tween: Tween<double>(begin: 1.0 , end: 0),
+        curve: Interval(0.7,1,curve:Curves.fastOutSlowIn),
+        builder: (BuildContext context , double _val , Widget child) {
+          return Transform(
+            transform: Matrix4.translationValues(0, _val * width, 0.0),
+            child: child,
+          );
+        }
     );
   }
 }
